d85c51f20c26a1d2d3675c2e40addcc23e2d442e
rafasu4 <raf59101@gmail.com>
2022-11-02 09:41:53 +0200

    📝documentation added

--- safe_call/safe_call.py
+++ safe_call/safe_call.py
@@ -1,5 +1,5 @@
 
-
+#handle the case when arguments with name are received
 def kwargs_handler(func, **kwargs) -> dict:
     kwargs_dic = dict()
     # holding all of the given arguments and their type
@@ -11,14 +11,18 @@ def kwargs_handler(func, **kwargs) -> dict:
             if value != dic[key]:
                 raise Exception('Wrong type parameters were entered!')
 
+#handle the case when arguments without name are received
 def args_handler(func, *args) -> tuple:
     args_tup = tuple()
     annot_tup = tuple()
+    #hold all the annotation types
     for v in func.__annotations__.values():
         if len(annot_tup) == len(args):
             break
         annot_tup += (v,)
+    #hold all of the parametes type
     for arg in args:
+        #hold only the number of parameters without name - handles the case we receive args & kwargs
         if(len(args_tup) == len(annot_tup)):
             break
         args_tup += (type(arg),)
@@ -26,20 +30,16 @@ def args_handler(func, *args) -> tuple:
         raise Exception('Wrong type parameters were entered!')
 
 def safe_call(func, *args, **kwargs):
+    #if we receive args & kwargs
     if len(kwargs) != 0 and len(args) != 0:
         kwargs_handler(func, **kwargs)
         args_handler(func, *args)
         return func(*args, **kwargs)
+    #if we receive only kwargs
     elif len(kwargs) != 0:
         kwargs_handler(func, **kwargs)
         return func(**kwargs)
+    #if we receive only args
     elif len(args) != 0:
         args_handler(func, *args)
         return func(*args)
-
-def stam_func(x:int , y:float, z):
-    return x+y+z
-
-
-if __name__ == "__main__":
-    print(safe_call(stam_func, 3, y=7.0, z=3))
